2329af4324c0401f6584c211c49b5c44c2a7071b
sam-trevone <trevonenigeria@gmail.com>
2023-06-20 12:00:19 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints alphabets
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] - "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+	putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
