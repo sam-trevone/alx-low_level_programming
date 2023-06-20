66bb7622d1da16a3649e4170656da0b24a33f12b
sam-trevone <trevonenigeria@gmail.com>
2023-06-20 12:13:54 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,14 @@
  */
 int main(void)
 {
-	char alp[26] - "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char c;
 
-	for (i = 0; i < 26; i++)
+	c = 'a'
+	while
+	(c <= 'z')
 	{
-	putchar(alp[i]);
+		putchar(c);
+		c++;
 	}
 	putchar('\n');
 	return (0);
