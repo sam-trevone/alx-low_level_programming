7fee1d32b13601a586150e20d4592e8aa2859c8e
sam-trevone <trevonenigeria@gmail.com>
2023-06-21 09:02:06 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets except q and e
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int n;
+	int m;
+
+	for (n = 0; n <= 98; n++)
+	{
+		for (m = n + 1; m <= 99; m++)
+		{
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			if (n == 98 && m == 99)
+			continue;
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
